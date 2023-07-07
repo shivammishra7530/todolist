e8661ad1de58e4145d6a65cabbda8b9c0a654570
shivammishra7530 <shivammishra7530@gmail.com>
2023-07-08 00:28:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+const[inputText, setInputText] = useState("");
+const [items, setItems] = useState([]);
+
+function handleChange(event){
+  const newValue = event.target.value;
+  setInputText(newValue);
+}
+
+function addItem(){
+  setItems((prevItems)=>{
+  return[...prevItems, inputText];
+  });
+setInputText("");
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container1">
+    <div className="container">
+    <div className="heading">
+     <h1>Make your List Here!</h1>
+     </div>
+
+    
+    <div className="input1">
+      
+      <input onChange={handleChange} type="text" value={inputText}></input>
+      
+      <button onClick={addItem}>
+        <span>Add</span>
+      </button>
+     
     </div>
+    <div>
+      <ul>
+      {items.map(todoItem => <li>{todoItem}</li>)}
+   
+      </ul>
+    </div>
+    </div>
+    </div>
+    
   );
 }
 
